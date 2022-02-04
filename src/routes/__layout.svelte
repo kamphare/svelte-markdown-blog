6186609899948832db2168b06ee35616e91cfa87
Anders Ugland <andersugland@anderss-macbook-pro.home>
2022-02-04 23:02:11 +0100

    complete base tutorial

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,12 +1,29 @@
+<script context="module">
+  export const load = ({ url }) => {
+    const currentRoute = url.pathname
+
+    return {
+      props: {
+        currentRoute
+      }
+    }
+  }
+</script>
+
 <script>
   import Header from '$lib/components/Header.svelte'
   import '$lib/css/styles.scss'
+  import { fade } from 'svelte/transition'
+
+export let currentRoute
 </script>
 
 <Header />
 
-<main>
-  <slot />
-</main>
+{#key currentRoute}
+  <main in:fade={{ duration: 300, delay: 150 }} out:fade={{ duration: 150 }}>
+    <slot />
+  </main>
+{/key}
 
 <footer>👣 You guessed it 👣</footer>
--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -1,7 +1,30 @@
-<h1>Blog</h1>
-<div class="egg">
-  <p>Here will be blog posts</p>
-</div>
+<script context="module">
+  export const load = async ({ fetch }) => {
+    const posts = await fetch('/api/posts.json')
+    const allPosts = await posts.json()
+
+    return {
+      props: {
+        posts: allPosts
+      }
+    }
+  }
+</script>
+
+<script>
+  export let posts; 
+</script>
+
+<ul>
+  { #each posts as post }
+    <li>
+      <h2>
+        <a href="{post.path}">{post.meta.title}</a>
+      </h2>
+      Published: {post.meta.date}
+    </li>
+  { /each }
+</ul>
 
 <style lang="scss">
 
